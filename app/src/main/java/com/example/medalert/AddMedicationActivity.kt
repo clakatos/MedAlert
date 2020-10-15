a267634a9976b76f593d3010cccef72836cbe8a0
Connor Lakatos <connor.lakatos1@gmail.com>
2020-10-15 12:55:09 -0400

    Add Medications Activity

--- app/src/main/java/com/example/medalert/AddMedicationActivity.kt
+++ app/src/main/java/com/example/medalert/AddMedicationActivity.kt
@@ -1,11 +1,119 @@
 package com.example.medalert
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.*
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddMedicationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_medication)
+
+        val medImage : LinearLayout = findViewById(R.id.medImage)
+        medImage.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+
+        ////////DOSAGE SPINNER////////////
+        val spinner: Spinner = findViewById(R.id.dosageSpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.dosage_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+        //////////////////////////////
+
+        /////////TEXT AUTOCOMPLETE FOR MEDICATIONS//////////////////////
+
+        // Get a reference to the AutoCompleteTextView in the layout
+        val textView = findViewById(R.id.medNameEdit) as AutoCompleteTextView
+// Get the string array
+        val countries: Array<out String> = resources.getStringArray(R.array.medications_array)
+// Create the adapter and set it to the AutoCompleteTextView
+        ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries).also { adapter ->
+            textView.setAdapter(adapter)
+        }
+
+        /////////////////////////////////////////////////////////////////
+
+        val saveIcon : ImageView = findViewById(R.id.addMedicationIcon)
+
+        saveIcon.setOnClickListener {
+            val medicationName : EditText = findViewById(R.id.medNameEdit)
+            val dosage : EditText = findViewById(R.id.dosageEdit)
+            val dosageAmt : Spinner = findViewById(R.id.dosageSpinner)
+            val startDate : EditText = findViewById(R.id.startEdit)
+            val endDate : EditText = findViewById(R.id.endEdit)
+            val doctor : EditText = findViewById(R.id.doctorEdit)
+            val directions : EditText = findViewById(R.id.directionsEdit)
+
+//            Log.d("INFO", medicationName.text.toString())
+//            Log.d("INFO", dosage.text.toString())
+//            Log.d("INFO", dosageAmt.selectedItem.toString())
+
+            val intent = Intent(this, MedicationsActivity::class.java)
+
+            startActivity(intent)
+        }
     }
+
+     private fun getMedicationInfo() {
+
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+        //createImageFile()
+    }
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            val imageBitmap = data.extras.get("data") as Bitmap
+//            ImageView.setImageBitmap(imageBitmap)
+//        }
+//    }
+
+
 }
--- app/src/main/java/com/example/medalert/MedicationsActivity.kt
+++ app/src/main/java/com/example/medalert/MedicationsActivity.kt
@@ -1,11 +1,36 @@
 package com.example.medalert
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 
 class MedicationsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medications)
+
+        val addedMed = false
+
+        val addMed_icon : ImageView = findViewById(R.id.addMedicationIcon)
+        addMed_icon.setOnClickListener {
+            !addedMed
+            openAddMedicationActivity()
+        }
+
+
+        //Log.d("INFO", findViewById(R.id.medNameEdit))
+        //val medNameText : TextView = findViewById(R.id.medNameEdit)
+        //val medNameElementBlock : TextView = findViewById(R.id.medNameText)
+        //medNameElementBlock.setText(medNameText.text.toString())
+
     }
+
+    private fun openAddMedicationActivity() {
+        val intent = Intent(this, AddMedicationActivity::class.java)
+        startActivity(intent)
+    }
+
 }
